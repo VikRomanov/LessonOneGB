63211f49c441efe348e3c2cc22e7d26ffc31ff8c
VikRomanov <vi.romanov@icloud.com>
2022-01-02 22:03:51 +0300

    Initial Commit

---
+++ LessonOneGB/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LessonOneGB
+//
+//  Created by macintosh on 02/01/2022.
+//  Copyright © 2022 macintosh. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
